46f4a748c51817453339602d0c58e92d9f34dd78
Denys <denis.itsme.1@gmail.com>
2024-07-27 13:26:38 +0300

    icons revision

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,33 +3,25 @@ export function recipeMarkup(recipes) {
     .map(
       ({ id, name, image, cookTimeMinutes, mealType }) => `
       <li class="recipes-list-item" data-id="${id}">
-              <img
-                class="recipe-list-item-image"
-                src="${image}"
-                alt="${name}"
-                width="368px"
-                loading="lazy"
-              />
-              <h3 class="recipe-list-item-title">${name}</h3>
-              <div class="recipe-info-wrapper">
-                <div class="recipe-cooking-time">
-                  <div class="recipe-cooking-time-icon">
-                    <svg width="24" height="24">
-                      <use href="./img/icons.svg#icon-timer"></use>
-                    </svg>
-                  </div>
-                  <p class="recipe-cooking-time-text">${cookTimeMinutes} Minutes</p>
-                </div>
-                <div class="recipe-meal-type">
-                  <div class="recipe-meal-type-icon">
-                    <svg width="24" height="24">
-                      <use href="./img/icons.svg#icon-forkKnife"></use>
-                    </svg>
-                  </div>
-                  <p class="recipe-meal-type-text">${mealType.join(', ')}</p>
-                </div>
-              </div>
-            </li>
+  <img
+    class="recipe-list-item-image"
+    src="${image}"
+    alt="${name}"
+    width="368px"
+    loading="lazy"
+  />
+  <h3 class="recipe-list-item-title">${name}</h3>
+  <div class="recipe-info-wrapper">
+    <div class="recipe-cooking-time">
+      <p class="recipe-cooking-time-icon">&#9200;</p>
+      <p class="recipe-cooking-time-text">${cookTimeMinutes} Minutes</p>
+    </div>
+    <div class="recipe-meal-type">
+      <p class="recipe-meal-type-icon">&#127860;</p>
+      <p class="recipe-meal-type-text">${mealType.join(', ')}</p>
+    </div>
+  </div>
+</li>
         `
     )
     .join('');
@@ -72,11 +64,9 @@ export function createRecipeModalWindow(recipe) {
     <p class="recipe-modal-cuisine">Cuisine: ${cuisine}</p>
   </div>
   <div class="recipe-modal-cooking-time">
-    <div class="recipe-cooking-time-icon">
-      <svg width="24" height="24">
-        <use href="./img/icons.svg#icon-timer"></use>
-      </svg>
-    </div>
+    <p class="recipe-cooking-time-icon">
+          &#9200;
+        </p>
     <p class="recipe-modal-cooking-time-text">
       Cooking time ${cookTimeMinutes} minutes
     </p>
@@ -96,14 +86,7 @@ export const markupHeroRecipesSliders = data => {
         `<div class="hero-recipes-swiper-slide">
   <div class="hero-slide-left-container">
     <div class="hero-random-recipe-mark">
-  <img
-    class="hero-random-recipe-mark-img"
-    src="../img/hero/recipe-mark@1x.png"
-    srcset="
-      ../img/hero/recipe-mark@1x.png 1x,
-      ../img/hero/recipe-mark@2x.png 2x
-    "
-  />
+  <p>&#128220;</p>
   <p class="hero-random-recipe-mark-text">Hot Recipes</p>
 </div>
     <h3 class="hero-random-recipe-title">${recipe.name}</h3>
@@ -113,21 +96,17 @@ export const markupHeroRecipesSliders = data => {
     </p>
     <div class="hero-random-recipe-info-wrapper">
       <div class="hero-random-recipe-info">
-        <div class="recipe-cooking-time-icon">
-          <svg width="24" height="24">
-            <use href="./img/icons.svg#icon-timer"></use>
-          </svg>
-        </div>
+        <p class="recipe-cooking-time-icon">
+          &#9200;
+        </p>
         <p class="hero-random-recipe-cooking-time-text">
           ${recipe.cookTimeMinutes} Minutes
         </p>
       </div>
       <div class="hero-random-recipe-info">
-        <div class="recipe-meal-type-icon">
-          <svg width="24" height="24">
-            <use href="./img/icons.svg#icon-forkKnife"></use>
-          </svg>
-        </div>
+        <p class="recipe-meal-type-icon">
+        &#127860;
+        </p>
         <p class="hero-random-recipe-meal-type-text">
           ${recipe.mealType.join(', ')}
         </p>
